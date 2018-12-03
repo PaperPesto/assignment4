ae92a26decfd67bf756fcbfda6079e4e5e1b2605
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-12-03 18:40:05 +0100

    clearInterval spenge il timer e a mano mando a null la reference, pronta ad essere rivalutata al prossimo start

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -9,6 +9,7 @@ export class GameControlComponent implements OnInit {
 
   @Output('counter') serverCreated = new EventEmitter<{ counter: number }>();
   counter: number;
+  setIntervalReference;
 
   constructor() { 
     this.counter = 0;
@@ -20,10 +21,26 @@ export class GameControlComponent implements OnInit {
   onStart(){
     console.log('Pressed start button');
 
-    setInterval(() => {
-      this.serverCreated.emit({counter: this.counter});
-      this.counter = this.counter + 1;
-      console.log('Incrementato counter:', this.counter);
-    }, 1000)
+    if(this.setIntervalReference){
+      console.log('setIntervalReference already opened at:', this.setIntervalReference);
+    }
+    else{
+      this.setIntervalReference = setInterval(() => {
+        this.serverCreated.emit({counter: this.counter});
+        this.counter = this.counter + 1;
+        console.log('Ref: %s - Counter: %s', this.setIntervalReference, this.counter);
+      }, 1000);
+  
+      console.log('opened setInterval reference:', this.setIntervalReference);
+    }
+  }
+
+  onStop(){
+    console.log('Pressed stop button');
+
+    clearInterval(this.setIntervalReference);
+    console.log('closed setInterval reference:', this.setIntervalReference);
+    this.counter = 0;
+    this.setIntervalReference = null;
   }
 }
